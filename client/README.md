b480da333e8dbed55b8cddbffd83b0e287b5d0ab
Shreya <shreyamasta99@gmail.com>
2024-12-31 22:37:02 +0530

    Initial Commit

---
+++ client/README.md
@@ -0,0 +1,42 @@
+# 🌐 My Portfolio Website
+
+Welcome to my personal portfolio website! This site showcases my projects, skills, and experiences as I continue my journey in the world of web development.
+
+## 🔗 Deployed URL
+
+## 🚀 Features
+
+- **Project Showcase**: A collection of my most impactful projects, complete with source code.
+- **About Me**: Learn more about who I am, my journey, and what drives me.
+- **Skills**: A breakdown of the technologies I used.
+- **Contact**: To get in touch with me.
+
+## 🔧 Tech Stack
+
+- **Frontend**: React
+- **Backend**: Node.js, Express.js, PostgreSQL, Javascript
+
+## 📚 How to Run Locally
+
+1. **Clone the repository**
+   ```bash
+   git clone https://github.com/Sherry-m03/My-Portfolio-Website.git
+   cd My-Portfolio-Website
+   ```
+2. **Install dependencies**
+   ```bash
+    npm install
+   ```
+3. **Run the development server**
+   ```bash
+   npm start
+   ```
+4. **Visit the site Open http://localhost:3000 in your browser.**
+
+## 📬 Contact
+
+</br>
+If you'd like to connect or discuss potential opportunities, feel free to reach out!
+
+- **Email:** shreyamasta99@gmail.com
+- **LinkedIn:** https://www.linkedin.com/in/shreya-masta/
---
+++ client/src/Contact.js
@@ -0,0 +1,55 @@
+import React, { useRef } from "react";
+
+export default function Contact() {
+  const form = useRef();
+
+  return (
+    <div id="contact">
+      <div className="portC-container">
+        <div className="portC-screen">
+          <div className="portC-screen-header">
+            <div className="portC-screen-header-left">
+              <div className="portC-screen-header-button portC-close"></div>
+              <div className="portC-screen-header-button portC-maximize"></div>
+              <div className="portC-screen-header-button portC-minimize"></div>
+            </div>
+            <div className="portC-screen-header-right">
+              <div className="portC-screen-header-ellipsis"></div>
+              <div className="portC-screen-header-ellipsis"></div>
+              <div className="portC-screen-header-ellipsis"></div>
+            </div>
+          </div>
+          <div className="portC-screen-body">
+            <div className="portC-screen-body-item portC-left">
+              <div className="portC-app-title">
+                <span>CONTACT</span>
+                <span>INFO</span>
+              </div>
+            </div>
+            <div className="portC-screen-body-item">
+              <div id="contact-info">
+                <img id="contact-icon" src="mail-icon.png"></img>
+                <p id="contact-details">shreyamasta99@gmail.com</p>
+              </div>
+              <div id="contact-info">
+                <img id="contact-icon" src="linkedin-icon.png"></img>
+                <a
+                  href="https://www.linkedin.com/in/shreya-masta/"
+                  id="contact-details"
+                >
+                  /shreya-masta
+                </a>
+              </div>
+              <div id="contact-info">
+                <img id="contact-icon" src="github-icon.png"></img>
+                <a href="https://github.com/Sherry-m03" id="contact-details">
+                  /Sherry-m03
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
